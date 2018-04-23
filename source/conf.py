11c79038c09e7985b3176f6c0af03ee9ce1fa783
matrix <matrix@matrixdeMacBook-Pro.local>
2018-04-23 16:58:42 +0800

    add html_theme_path

--- source/conf.py
+++ source/conf.py
@@ -88,6 +88,10 @@ html_theme = 'rosefinch'
 #
 # html_theme_options = {}
 
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = ['.']
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
